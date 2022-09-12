951ac94cbc274bacc00046c1651072b10146d32f
ShaunHalliday <shaun.halliday@ciptex.com>
2022-09-12 09:56:24 +0100

    chore: Rename copy
    video chat set to Video Chat - capitalised to match others

--- race-client-sdk-react-demo/src/components/RaceConnect/index.tsx
+++ race-client-sdk-react-demo/src/components/RaceConnect/index.tsx
@@ -62,7 +62,7 @@ export const RaceConnect: FC = () => {
 							<NavButton icon="call" title="Call" onClick={() => { setContainerName("call"); setButtonContainer(false); }}/>
 							<NavButton icon="chat" view="0 0 576 512" title="Chat" onClick={() => { setContainerName("chat"); setButtonContainer(false); }}/>
 							<NavButton icon="enquire" title="Enquire" onClick={() => { setContainerName("enquire"); setButtonContainer(false); }}/>
-							<NavButton icon="video" view="0 0 576 512" title="video Chat" onClick={() => { setContainerName("video"); setButtonContainer(false); }}/>
+							<NavButton icon="video" view="0 0 576 512" title="Video Chat" onClick={() => { setContainerName("video"); setButtonContainer(false); }}/>
 							<NavButton icon="whatsapp" view="0 0 448 512" title="WhatsApp" onClick={() => { setContainerName("whatsapp"); setButtonContainer(false); }}/>
 							<NavButton icon="facebook" title="Facebook Messenger" onClick={() => { setContainerName("facebook"); setButtonContainer(false); }}/>
 						</ButtonsContainer>
