02aecb4c1cb526e61ad1f380e42de9b94ef08b06
SinaTaavon <32191839+SinaTaavon@users.noreply.github.com>
2022-11-28 12:04:51 +0000

    chore: error toast has been added

--- race-client-sdk-react-demo/src/components/IframeLoader/IframeLoader.tsx
+++ race-client-sdk-react-demo/src/components/IframeLoader/IframeLoader.tsx
@@ -1,61 +1,77 @@
-import { Anchor, Box, Button, HelpText, Input, Label } from "@twilio-paste/core";
+import { Anchor, Box, Button, HelpText, Input, Label, Toaster, useToaster } from "@twilio-paste/core";
 import {  FC, useState } from "react";
 
 
 export const IframeLoader: FC = () => {
 
+	const toaster = useToaster();
 	const [url, setUrl] = useState<string>("");
 
 	const handleChange = (e: any) => {
-		if (e.target.value.match(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/)) {
-			setUrl(e.target.value);
-		} else {
-			setUrl("");
-		}
+		setUrl(e.target.value);
 	}
 
-	const handleSubmit = () => {
-		const iframeOld = document.getElementById("iframe");
-		if (iframeOld) {
-			iframeOld.remove();
-			const iframe = document.createElement("iframe");
-			iframe
-				.setAttribute("src", url);
-			iframe
-				.setAttribute("frameborder", "0");
-			iframe
-				.setAttribute("allowfullscreen", "true");
-			iframe
-				.setAttribute("width", "100%");
-			iframe
-				.setAttribute("height", "100%");
-			iframe
-				.setAttribute("id", "iframe");
-			const iframeContainer = document.getElementById("iframe_container");
-			if (iframeContainer) {
-				iframeContainer
-					.appendChild(iframe);
-			}
+	// create a function to check url is valid or not
+	const isValidUrl = (string: string) => {
+		try {
+			new URL(string);
+		} catch (_) {
+			return false;
 		}
-		else {
-			const iframe = document.createElement("iframe");
-			iframe
-				.setAttribute("src", url);
-			iframe
-				.setAttribute("frameborder", "0");
-			iframe
-				.setAttribute("allowfullscreen", "true");
-			iframe
-				.setAttribute("width", "100%");
-			iframe
-				.setAttribute("height", "100%");
-			iframe
-				.setAttribute("id", "iframe");
-			const iframeContainer = document.getElementById("iframe_container");
-			if (iframeContainer) {
-				iframeContainer
-					.appendChild(iframe);
+		return true;
+	}
+
+
+	const handleSubmit = () => {
+		if (isValidUrl(url)) {
+			const iframeOld = document.getElementById("iframe");
+			if (iframeOld) {
+				iframeOld.remove();
+				const iframe = document.createElement("iframe");
+				iframe
+					.setAttribute("src", url);
+				iframe
+					.setAttribute("frameborder", "0");
+				iframe
+					.setAttribute("allowfullscreen", "true");
+				iframe
+					.setAttribute("width", "100%");
+				iframe
+					.setAttribute("height", "100%");
+				iframe
+					.setAttribute("id", "iframe");
+				const iframeContainer = document.getElementById("iframe_container");
+				if (iframeContainer) {
+					iframeContainer
+						.appendChild(iframe);
+				}
 			}
+			else {
+				const iframe = document.createElement("iframe");
+				iframe
+					.setAttribute("src", url);
+				iframe
+					.setAttribute("frameborder", "0");
+				iframe
+					.setAttribute("allowfullscreen", "true");
+				iframe
+					.setAttribute("width", "100%");
+				iframe
+					.setAttribute("height", "100%");
+				iframe
+					.setAttribute("id", "iframe");
+				const iframeContainer = document.getElementById("iframe_container");
+				if (iframeContainer) {
+					iframeContainer
+						.appendChild(iframe);
+				}
+			}
+		} else {
+			toaster.push({
+				message: "URL should start with https://",
+				variant: "error",
+				dismissAfter: 5000
+			})
 		}
 	}
 
@@ -72,6 +88,7 @@ export const IframeLoader: FC = () => {
 				<HelpText id="url">* Please make sure the website you are trying to view allows iframe loading. <Anchor href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options" target="_blank">Learn more here</Anchor>.</HelpText>
 			</Box>
 			<Box id="iframe_container" height="2000px"></Box>
+			<Toaster {...toaster} />
 		</>
 	)
 }
